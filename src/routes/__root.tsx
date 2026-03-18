b7d9ec8ef672208cc0db03617812a358c6b34ecb
younes101020 <senyoupl@icloud.com>
2026-03-18 21:43:26 +0100

    fix: disable devtools on prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -37,17 +37,19 @@ function RootDocument({ children }: { children: React.ReactNode }) {
 			</head>
 			<body className="h-full">
 				{children}
-				<TanStackDevtools
-					config={{
-						position: "bottom-right",
-					}}
-					plugins={[
-						{
-							name: "Tanstack Router",
-							render: <TanStackRouterDevtoolsPanel />,
-						},
-					]}
-				/>
+				{import.meta.env.DEV && (
+					<TanStackDevtools
+						config={{
+							position: "bottom-right",
+						}}
+						plugins={[
+							{
+								name: "Tanstack Router",
+								render: <TanStackRouterDevtoolsPanel />,
+							},
+						]}
+					/>
+				)}
 				<Scripts />
 			</body>
 		</html>
