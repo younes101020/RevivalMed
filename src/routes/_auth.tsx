b8309a89bfe288f600f50dd48201bf90d6829bfe
younes101020 <senyoupl@icloud.com>
2026-04-06 22:56:57 +0200

    fix(routes): restore route components overwritten by TanStack Router generator

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -1,9 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect, Outlet } from "@tanstack/react-router";
+import { Nav } from "@/components/layout/nav";
 
-export const Route = createFileRoute('/_auth')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/_auth")({
+beforeLoad: ({ context }) => {
+if (!context.user) {
+throw redirect({ to: "/login" });
+}
+},
+component: AuthLayout,
+});
 
-function RouteComponent() {
-  return <div>Hello "/_auth"!</div>
+function AuthLayout() {
+return (
+<div className="h-full flex flex-col">
+<Nav />
+<main className="flex-1 overflow-auto">
+<Outlet />
+</main>
+</div>
+);
 }
